2ec7d94313288fa8001a3481f23118fb82213d82
Robert <gusroberth@student.gu.se>
2019-02-26 22:18:43 +0100

    updating

--- vxml-server/app.py
+++ vxml-server/app.py
@@ -5,21 +5,21 @@ temp = '13'
 
 @app.route('/lab1')
 def lab1():
-    vxml = render_template('lab1.xml')
+    vxml = render_template('lab1/lab1.xml')
     response = make_response(vxml)
     response.headers["Content-Type"] = "application/xml"
     return response
 
 @app.route('/lab2')
 def lab2():
-    vxml = render_template('lab2.xml')
+    vxml = render_template('lab2/lab2.xml')
     response = make_response(vxml)
     response.headers["Content-Type"] = "application/xml"
     return response
 
 @app.route('/lab3')
 def lab3():
-    vxml = render_template('lab3.xml')
+    vxml = render_template('lab3/lab3.xml')
     response = make_response(vxml)
     response.headers["Content-Type"] = "application/xml"
     return response
@@ -30,21 +30,21 @@ def send_grammar(path):
 
 @app.route('/delayed_flights')
 def delayed_flights():
-    vxml = render_template('delayed_flights.xml', temp=temp)
+    vxml = render_template('lab1/delayed_flights.xml', temp=temp)
     response = make_response(vxml)
     response.headers["Content-Type"] = "application/xml"
     return response
 
 @app.route('/life_advice')
 def life_advice():
-    vxml = render_template('life_advice.xml', temp=temp)
+    vxml = render_template('lab1/life_advice.xml', temp=temp)
     response = make_response(vxml)
     response.headers["Content-Type"] = "application/xml"
     return response
 
 @app.route('/flight_booking')
 def flight_booking():
-    vxml = render_template('flight_booking.xml', temp=temp)
+    vxml = render_template('lab1/flight_booking.xml', temp=temp)
     response = make_response(vxml)
     response.headers["Content-Type"] = "application/xml"
     return response
