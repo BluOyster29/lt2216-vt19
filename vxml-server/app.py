e6be686c30a817f328b7d814d208cc1411bbbee8
Robert <gusroberth@student.gu.se>
2019-02-26 21:00:48 +0100

    updating app.py

--- vxml-server/app.py
+++ vxml-server/app.py
@@ -3,13 +3,6 @@ app = Flask(__name__)
 
 temp = '13'
 
-@app.route('/weather')
-def hello():
-    vxml = render_template('weather.xml', temp=temp)
-    response = make_response(vxml)
-    response.headers["Content-Type"] = "application/xml"
-    return response
-
 @app.route('/lab1')
 def lab1():
     vxml = render_template('lab1.xml')
